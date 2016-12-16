a96f4f57467c3eb48bbbe7ce41341f63e0c9dbbd
Phil Cable <pcable@wsu.edu>
2016-12-16 09:06:37 -0800

    Make adjustments to meet coding standards

--- src/js/cycle.js
+++ src/js/cycle.js
@@ -1,30 +1,16 @@
-(function ($) {
-	'use strict';
-
-	// Carousel
-	// Add the 'ready' class to the carousel after the page is loaded.
-	// This class is used in the selector for the CSS transitions
-	// to prevent them from firing while the page is loading.
-	/*$(window).load(function () {
-		$('.carousel').addClass('ready');
-	});*/
+( function( $ ) {
+	"use strict";
 
 	// Override carousel defaults.
-	$('.new.cycle-slideshow').cycle({
-		//fx: 'carousel',
-        //visible: 5,
-        //'carousel-offset': 2,
-        next: '.new-next',
-        prev: '.new-prev'
-    });
-    
-    // Override carousel defaults.
-	$('.best.cycle-slideshow').cycle({
-		//fx: 'carousel',
-        //visible: 5,
-        //'carousel-offset': 2,
-        next: '.best-next',
-        prev: '.best-prev'
-    });
-    
-}(jQuery));
+	$( ".new.cycle-slideshow" ).cycle( {
+		next: ".new-next",
+		prev: ".new-prev"
+	} );
+
+	// Override carousel defaults.
+	$( ".best.cycle-slideshow" ).cycle( {
+		next: ".best-next",
+		prev: ".best-prev"
+	} );
+
+}( jQuery ) );
