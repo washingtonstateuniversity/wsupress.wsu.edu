864572e341b6edc6a8887f7092a66b7e5cd5e50c
Phil Cable <pcable@wsu.edu>
2016-12-19 14:20:50 -0800

    Add cycle.js functions so the slideshows initialize

--- src/js/cycle.js
+++ src/js/cycle.js
@@ -3,14 +3,26 @@
 
 	// Override carousel defaults.
 	$( ".new.cycle-slideshow" ).cycle( {
+		fx: "fade",
+		timeout: 0,
+		slides: "div",
 		next: ".new-next",
-		prev: ".new-prev"
+		prev: ".new-prev",
+		log: false,
+		"carousel-fluid": true,
+		"carousel-visible": 1
 	} );
 
 	// Override carousel defaults.
 	$( ".best.cycle-slideshow" ).cycle( {
+		fx: "fade",
+		timeout: 0,
+		slides: "div",
 		next: ".best-next",
-		prev: ".best-prev"
+		prev: ".best-prev",
+		log: false,
+		"carousel-fluid": true,
+		"carousel-visible": 1
 	} );
 
 }( jQuery ) );
