3f37a745815a6616059f67d90a0b0718eae61838
Phil Cable <pcable@wsu.edu>
2017-02-02 15:01:40 -0800

    Adjust spacing for coding standards

--- functions.php
+++ functions.php
@@ -28,8 +28,8 @@ function wsu_press_enqueue_scripts() {
 // WooCommerce Hooks
 remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
 remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10 );
-add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10 );
-add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10 );
+add_action( 'woocommerce_before_main_content', 'my_theme_wrapper_start', 10 );
+add_action( 'woocommerce_after_main_content', 'my_theme_wrapper_end', 10 );
 
 function my_theme_wrapper_start() {
 	echo '<div class="column one">';
