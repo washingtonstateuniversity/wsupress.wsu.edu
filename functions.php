de3f6c4d5f5ad5bd788b8fcdcb17c660a583522a
jbickelhaupt <jbickelhaupt@wsu.edu>
2017-02-02 09:27:12 -0800

    WooCommerce Content Compatibility
    Adding custom hooks and declaring WooCommerce support for theme

--- functions.php
+++ functions.php
@@ -23,4 +23,23 @@ function wsu_press_enqueue_scripts() {
         wp_enqueue_script( 'cycle_carousel', get_stylesheet_directory_uri() . '/js/jquery.cycle2.carousel.min.js', array( 'wsu-cycle' ), false, true );
 		wp_enqueue_script( 'cycle_scripts', get_stylesheet_directory_uri() . '/js/cycle.js', array( 'cycle_carousel' ), false, true );
 	}
+// WooCommerce Hooks
+    remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+    remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+    add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
+    add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
+
+    function my_theme_wrapper_start() {
+        echo '<div class="column one">';
+    }
+
+    function my_theme_wrapper_end() {
+        echo '</div>';
+    }
+// Declare WooCommerce Support
+    add_action( 'after_setup_theme', 'woocommerce_support' );
+    
+    function woocommerce_support() {
+        add_theme_support( 'woocommerce' );
+    }
 }
