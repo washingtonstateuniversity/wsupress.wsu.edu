c6beeb488ad1a4120e4f5e494cae90996d7e89f3
jbickelhaupt <jbickelhaupt@wsu.edu>
2017-02-02 15:32:37 -0800

    Remove WooCommerce Hooks, Add Page Template
    removed custom WC hooks from function file; added WC page file

--- functions.php
+++ functions.php
@@ -25,19 +25,6 @@ function wsu_press_enqueue_scripts() {
 	}
 }
 
-// WooCommerce Hooks
-remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
-remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10 );
-add_action( 'woocommerce_before_main_content', 'my_theme_wrapper_start', 10 );
-add_action( 'woocommerce_after_main_content', 'my_theme_wrapper_end', 10 );
-
-function my_theme_wrapper_start() {
-	echo '<div class="column one">';
-}
-
-function my_theme_wrapper_end() {
-	echo '</div>';
-}
 // Declare WooCommerce Support
 add_action( 'after_setup_theme', 'woocommerce_support' );
 
