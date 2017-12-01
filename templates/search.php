838a0cde72c03ebac66df618a05e1073bb025c48
Phil Cable <pcable@wsu.edu>
2017-12-01 15:36:16 -0800

    Output featured images in the search results template
    There's almost certainly a smarter way to do this...

--- templates/search.php
+++ templates/search.php
@@ -23,23 +23,36 @@ get_header();
 			</section>
 			<section class="row side-right gutter pad-ends search-results-container">
 
-				<div class="column one">
+				<div class="column one deck deck--results">
 					<?php
 
 					if ( empty( $search_results ) ) {
 						?><h2>No search results found.</h2><?php
 					}
 					foreach ( $search_results as $search_result ) {
-						?>
-						<article>
-						<h2><a href="<?php echo esc_url( $search_result->_source->url ); ?>"><?php echo esc_html( $search_result->_source->title ); ?></a></h2>
-						<div class="visible-content">
-							<?php
-							$visible_content = WSU\Press\Search\filter_elastic_content( $search_result->_source->content );
 
-							echo wp_kses_post( $visible_content );
-							?>
-						</div>
+						$result_post = get_page_by_path( basename( $search_result->_source->url ), OBJECT, $search_result->_source->post_type );
+						?>
+						<article class="card card--result">
+
+							<?php if ( $result_post && has_post_thumbnail( $result_post->ID ) ) { ?>
+							<figure class="card-image">
+								<a href="<?php echo esc_url( $search_result->_source->url ); ?>">
+									<?php echo get_the_post_thumbnail( $result_post->ID, array( 200, 300 ) ); ?>
+								</a>
+							</figure>
+							<?php } ?>
+
+							<h2><a href="<?php echo esc_url( $search_result->_source->url ); ?>"><?php echo esc_html( $search_result->_source->title ); ?></a></h2>
+
+							<div class="visible-content">
+								<?php
+								$visible_content = WSU\Press\Search\filter_elastic_content( $search_result->_source->content );
+
+								echo wp_kses_post( $visible_content );
+								?>
+							</div>
+
 						</article><?php
 					}
 					?>
